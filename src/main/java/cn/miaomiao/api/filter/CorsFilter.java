e9cb6f51cf35c15b19402f60bac9231eac67a8bc
amiao <353639110@qq.com>
2019-05-28 15:09:25 +0800

    修改跨域信息

--- src/main/java/cn/miaomiao/api/filter/CorsFilter.java
+++ src/main/java/cn/miaomiao/api/filter/CorsFilter.java
@@ -37,7 +37,7 @@ public class CorsFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Headers", "authToken, content-type");
         // 允许跨域的请求方法类型
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "*");
         // 预检命令（OPTIONS）缓存时间，单位：秒
         response.setHeader("Access-Control-Max-Age", "3600");
 
